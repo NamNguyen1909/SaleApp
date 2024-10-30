f5b1a0f9620b07df59d687221a717976710bb302
Thanh Nam <namnguyen19092004@gmail.com>
2024-10-30 16:32:59 +0700

    conect mysql with sqlalchemy

---
+++ SaleApp/app/index.py
@@ -0,0 +1,16 @@
+from flask import render_template
+import dao
+from SaleApp.app import app
+
+
+@app.route ("/")
+
+def index():
+    cates=dao.load_categories()
+    prods=dao.load_products()
+    return render_template('index.html',categories=cates,products=prods)
+
+
+if __name__== '__main__':
+    with app.app_context():
+        app.run(debug=True)
